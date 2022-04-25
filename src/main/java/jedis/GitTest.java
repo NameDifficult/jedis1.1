261542d790b6c982e685343e264199542c69f391
fujialiang <fjl2212245373@qq.com>
2022-04-25 17:16:38 +0800

    jedis 3.0

--- src/main/java/jedis/GitTest.java
+++ src/main/java/jedis/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("testgit");
         System.out.println("helloone");
+        System.out.println("hello");
     }
 }
